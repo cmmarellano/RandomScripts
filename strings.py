f76128519da1e26f34f3d373ccfc860f031c4188
Carla Arellano <cmarellano@up.edu.ph>
2022-04-24 00:30:39 +0200

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 24 00:24:02 2022
+
+@author: Arellano
+"""
+
+import sys
+
+
+# Read argument passed
+input_str = sys.argv[1]
+exclam = "!!!"
+
+
+# Return output str
+output_str = input_str.upper() + exclam
+print(output_str)
