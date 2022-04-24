e5c583d6cd7366feffb8c24edbdc7e5ffcc2e487
Carla Arellano <cmarellano@up.edu.ph>
2022-04-24 18:17:27 +0200

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 24 18:14:28 2022
+TwilioQuest: The Pythonic Temple
+    Python Initiation: Fizzbuzz
+    Trial of Trickery
+@author: Arellano
+"""
+import sys
+
+args = sys.argv
+args.pop(0)
+#test print
+print(f"args:  {args}")
