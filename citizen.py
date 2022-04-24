e7b58e37736d785ccdd63f45574e668ce62faefb
Carla Arellano <cmarellano@up.edu.ph>
2022-04-24 16:51:54 +0200

    Create citizen.py

---
+++ citizen.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 24 16:00:46 2022
+TwilioQuest: The Pythonic Temple
+    Python Initiation: Create Python class
+@author: Arellano
+"""
+
+
+import sys
+
+
+#Creating Python class to describe a citizen of Python
+class Citizen:
+    """
+    Class that describes a citizen.
+    Requires first name and last name.
+    
+    """
+    
+    
+    # Class variable shared by all instances
+    greeting = "For the glory of Python!"
+    
+    # init method
+    def __init__(self, first_name, last_name):
+        #argument to instance variables
+        self.first = first_name  
+        self.last = last_name
+        
+     #instance method  
+    def full_name(self):
+        self.name = self.first + " " + self.last
+        print(self.name)
+        return (self.name)
+
+
+# Execute class and  call arguments
+x = Citizen(sys.argv[1], sys.argv[2])
+print(x.greeting)
+x.full_name()
+
