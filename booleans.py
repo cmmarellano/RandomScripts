f9ec634765294d0ce7b0fc56632a8c14be0767b1
Carla Arellano <cmarellano@up.edu.ph>
2022-04-24 14:37:00 +0200

    Create booleans.py

---
+++ booleans.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 24 14:22:47 2022
+TwilioQuest: The Pythonic Temple
+    Python Initiation: Booleans
+@author: Arellano
+"""
+
+
+# Boolean variables declaration
+python_is_glorious = True
+failure_is_option = False
+proper_greeting = False
