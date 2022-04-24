4675ad017afd312c8ca50ca5857219bc3388a191
Carla Arellano <cmarellano@up.edu.ph>
2022-04-24 11:59:11 +0200

    Update list_iteration.py

--- list_iteration.py
+++ list_iteration.py
@@ -10,13 +10,14 @@ import sys
 
 # Create list
 order_of_succession = sys.argv
+# pass on the first sys.argv
+order_of_succession.pop(0)
 
 
+# For loop for every item, together with their index 
 print("Leaders:")
 for index, item in enumerate(order_of_succession, start=1):
     str_print = f"{index}. {item}"
     print(str_print)
     
     
-    
-order_of_succession.pop(0)
