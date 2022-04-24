382eb145a926dc1fc8a217f1ba963c3bd9f0df10
Carla Arellano <cmarellano@up.edu.ph>
2022-04-24 13:41:41 +0200

    functions 1 and 2
    Configure to add new function called "add numbers"

--- functions.py
+++ functions.py
@@ -7,8 +7,21 @@ Created on Sun Apr 24 12:01:25 2022
 
 import sys
 
+#arg1 = sys.argv[1] + " " + sys.argv[2]
 arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+
+
+
 def hail_friend(args):
     print(f"Hail, {args}!")
 
-hail_friend(arg1)
+
+def add_numbers(num1, num2):
+    result_sum = arg1 + arg2
+    return result_sum
+
+
+# Call function
+#hail_friend(arg1)
+add_numbers(arg1, arg2)
